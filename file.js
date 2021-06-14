aa4d0c36be32b0987b9a2fc642aa534abdba0a0d
EmilianoMar <65531354+EmilianoMar@users.noreply.github.com>
2021-06-14 14:29:54 +0200

    Update file.js

--- file.js
+++ file.js
@@ -10,15 +10,12 @@ var div9 = document.getElementById("div9");
 var div10 = document.getElementById("div10");
 var div11 = document.getElementById("div11");
 var fine = false;
-var fine2 = false;
 var btn = document.getElementById("btn");
 var doge = document.getElementById("doge");
 var body = document.getElementById("Body");
 var skull = document.getElementById("skull");
 
 function hack(){
-   if{!fine2
-   fine2 = true;
    div.style.visibility = "hidden";
    div2.style.visibility = "visible";
    btn.style.visibility = "hidden";
@@ -30,7 +27,6 @@ function hack(){
          return;
       })
    },1000)
-   }
 }
 
 function hack2(){
